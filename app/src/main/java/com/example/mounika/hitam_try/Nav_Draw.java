9ccb78cd8e0f3cb79fc83e80d7703d7963cba75b
jayanth539 <jayanthpathakoti@gmail.com>
2018-07-31 19:45:36 +0530

    Next COmmit

--- app/src/main/java/com/example/mounika/hitam_try/Nav_Draw.java
+++ app/src/main/java/com/example/mounika/hitam_try/Nav_Draw.java
@@ -26,6 +26,14 @@ public class Nav_Draw extends AppCompatActivity
 
 
 
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -41,6 +49,18 @@ public class Nav_Draw extends AppCompatActivity
         getMenuInflater().inflate(R.menu.nav__draw, menu);
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
