2f3041f777acb007c43328cd6164d79cdc23c8ae
Jayanth <jayanthpathakoti@gmail.com>
2018-08-23 20:05:42 +0530

    After Adding the progress bar.

--- app/src/main/java/com/example/mounika/hitam_try/MainActivity.java
+++ app/src/main/java/com/example/mounika/hitam_try/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.mounika.hitam_try;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView iv;
+    private ProgressBar mProgress;
+    private Thread timer;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,43 +32,37 @@ public class MainActivity extends AppCompatActivity {
         final Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
         iv.startAnimation(myanim);
         final Intent homeIntent = new Intent(MainActivity.this, Nav_Draw.class);
-        final Thread timer = new Thread() {
+
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+
+
+        mProgress.setProgressTintList(ColorStateList.valueOf(Color.BLACK));
+       timer = new Thread() {
             public void run() {
 
-                try {
+                doWork();
 
-                    sleep(5000);
-
-
-                } catch (Exception c) {
-                    c.printStackTrace();
-
-                } finally {
-                    startActivity(homeIntent);
-                    finish();
-                }
+                startActivity(homeIntent); finish();
             }
         };
 
        timer.start();
 
 
-
-       /* long SPLASH_TIME_OUT=400;
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run(){
-
-
-                Intent homeIntent = new Intent(MainActivity.this,Nav_Draw.class);
-                startActivity(homeIntent);
-               finish();
-            }
-
-        },SPLASH_TIME_OUT);
-       */
-
-
     }
+
+    private void doWork() {
+
+            for(int i=0 ; i<100 ; i+= 10 ){
+                i+= new Random().nextInt(10);
+                try {
+                    timer.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mProgress.setProgress(i);
+
+            }
+        }
+
 }
